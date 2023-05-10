2611bc943dd6c0ffdbd630bfac07bc695ec5b193
austinmarckxUTSW <austin.marckx@utsouthwestern.edu>
2023-05-10 08:52:49 -0500

    fix name of VAEGAN env

--- installVAEGANEnv.sh
+++ installVAEGANEnv.sh
@@ -1,5 +1,5 @@
 # install Day 1 environment into jupyter 
 module add python
 conda activate /archive/course/SWE22/shared/week2/CondaEnvs/VAE_GAN_env_new
-python -m ipykernel install --name VAE_GAN_env_new --user --display-name "VAE_GAN_env_new"
+python -m ipykernel install --name VAE_GAN_env_22 --user --display-name "VAE_GAN_env_22"
 conda deactivate
