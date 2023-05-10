ade32a3bf23088065da6d08ee61cf35ed80843c9
austinmarckxUTSW <austin.marckx@utsouthwestern.edu>
2023-05-10 08:32:57 -0500

    Revert VAEGAN env path to last year's folder

--- activateVAEGANEnv.sh
+++ activateVAEGANEnv.sh
@@ -5,4 +5,4 @@ module add cudnn/7.6.5.32
 module add gcc
 # python 3.8.12
 module add python    
-conda activate /archive/course/SWE22/shared/SWE2023_week2/CondaEnvs/VAE_GAN_env_new
+conda activate /archive/course/SWE22/shared/week2/CondaEnvs/VAE_GAN_env_new
